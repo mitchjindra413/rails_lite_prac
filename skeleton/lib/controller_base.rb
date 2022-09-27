3c08803c163c5d003c4f9391ed86b311452b8b07
AA Student <89243713+jason330@users.noreply.github.com>
2022-09-26 17:28:31 -0700

    write render_content

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,10 +8,13 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @request = req
+    @response = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response 
   end
 
   # Set the response status code and header
@@ -22,6 +25,12 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    # env["Content-type"] = content_type
+    # env["Content"] = content
+    @response.content_type = content_type
+    @response.write(content)
+    raise "double render error" if already_built_response?
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
